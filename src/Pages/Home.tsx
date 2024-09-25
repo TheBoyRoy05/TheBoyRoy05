67c8097cc8efb095a514f42dcf7c8b6c3bea34a6
Issac Roy <issacroy05@gmail.com>
2024-09-24 23:57:26 -0700

    Feat: Added custom scrollbar and fade from landing page, closes #2

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,15 +1,31 @@
+import { FaArrowDownLong } from "react-icons/fa6";
 import Scene from "../Components/Boids/Scene";
 import Navbar from "../Components/Navbar";
 import TypingText from "../Components/TypingText";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
+import { useRef } from "react";
 
 const Home = () => {
+  const mainPage = useRef<HTMLDivElement>(null!);
+
+  const handleClick = () => {
+    if (mainPage) mainPage.current.scrollIntoView({behavior: 'smooth'});
+  }
+
   return (
     <>
       <Scene />
-      <div className="absolute left-0 top-0 w-screen h-screen">
+      <SimpleBar className="absolute left-0 top-0 w-screen h-screen overflow-x-hidden">
         <Navbar />
         <TypingText />
-      </div>
+        <div className="mt-20 h-64 bg-gradient-to-b from-transparent to-gray-800 flex justify-center items-center">
+          <button onClick={handleClick}>
+            <FaArrowDownLong className="text-3xl mt-20" />
+          </button>
+        </div>
+        <div className="h-[1000px] bg-gray-800" ref={mainPage}></div>
+      </SimpleBar>
     </>
   );
 };
