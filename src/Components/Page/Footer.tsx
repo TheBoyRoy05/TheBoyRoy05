099c494634b1242dfdd789f65cd6e9c3e95f5dc0
Issac Roy <issacroy05@gmail.com>
2024-09-28 22:15:39 -0700

    Feat: Added last commit time to footer

--- src/Components/Page/Footer.tsx
+++ src/Components/Page/Footer.tsx
@@ -2,13 +2,14 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa6";
 import { IoDocumentText } from "react-icons/io5";
 import { SiGmail } from "react-icons/si";
 import { useNavigate } from "react-router-dom";
+import LastUpdated from "./LastUpdate";
 
 const Footer = () => {
   const navigate = useNavigate();
   const btnClass =
     "btn rounded-full text-white text-lg w-12 p-0 border-none bg-[#202020] hover:bg-[#282828] glow";
 
-  const handleMail = (e: { preventDefault: () => void; }) => {
+  const handleMail = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     const mailtoLink = `mailto:issacroy05@gmail.com`;
     window.location.href = mailtoLink;
@@ -16,23 +17,26 @@ const Footer = () => {
 
   return (
     <div className="border-t border-[--border-color] mt-[5vw] flex py-8 bg-[#181818]">
-      <div className="flex flex-col flex-1 gap-6 py-4 items-center">
+      <div className="flex flex-col flex-1 gap-3 items-center">
         <div className="text-[3rem] font-bold">
           <span className="text-white">{"Issac "}</span>
           <span className="text-[--hero-text]">{"Roy"}</span>
         </div>
-        <span className="">{"© 2024 Issac Roy, All Rights Reserved"}</span>
+        <div className="flex flex-col gap-2 items-center text-sm font-medium">
+          <span>{"© 2024 Issac Roy, All Rights Reserved"}</span>
+          <LastUpdated />
+        </div>
       </div>
-      <div id="quick-link" className="flex gap-x-20 border-x border-[--border-color] px-32">
+      <div id="quick-link" className="flex flex-1 justify-center gap-x-20 border-x border-[--border-color]">
         <div className="flex flex-col gap-y-2 justify-center">
           <h4 className="text-lg font-bold mb-2">{"Info"}</h4>
-          <a href="/about-me">{"About"}</a>
-          <a href="/contact-me">{"Contact"}</a>
+          <a href="/about-me" className="hover:underline">{"About"}</a>
+          <a href="/contact-me" className="hover:underline">{"Contact"}</a>
         </div>
         <div className="flex flex-col gap-y-2 justify-center">
           <h4 className="text-lg font-bold mb-2">{"Work"}</h4>
-          <a href="/projects">{"Projects"}</a>
-          <a href="/blog">{"Blog"}</a>
+          <a href="/projects" className="hover:underline">{"Projects"}</a>
+          <a href="/blog" className="hover:underline">{"Blog"}</a>
         </div>
       </div>
       <div
@@ -58,11 +62,7 @@ const Footer = () => {
           >
             <FaLinkedinIn />
           </button>
-          <button
-            onClick={() => navigate(`/resume`)}
-            className={btnClass}
-            title={"Resume"}
-          >
+          <button onClick={() => navigate(`/resume`)} className={btnClass} title={"Resume"}>
             <IoDocumentText />
           </button>
         </div>
