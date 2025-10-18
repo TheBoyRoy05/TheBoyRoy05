9c11bbd106f8e0f6c34db4c2265ac6a595621e5b
Issac Roy <issacroy05@gmail.com>
2025-10-18 11:29:30 -0700

    Fix?: Rounting with hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import App from "./App.tsx";
 import "./Styles/fonts.css";
@@ -9,9 +9,9 @@ import "./Styles/animations.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <Toaster />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
