f5db078eb992a5c90b6ec928ca5834d0358cbbe3
Issac Roy <issacroy05@gmail.com>
2025-12-28 15:01:17 -0800

    added view all button

--- src/Components/Blog/Blog.tsx
+++ src/Components/Blog/Blog.tsx
@@ -1,13 +1,16 @@
 import { articles } from "../../Assets/Data/articles";
 import BlogCard from "./BlogCard";
+import { useNavigate } from "react-router-dom";
 
 interface BlogProps {
   upTo?: number;
 }
 
 const Blog = ({ upTo = articles.length }: BlogProps) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex-col items-center gap-10">
+    <div className="flex-col items-center gap-10 pb-[15vh]">
       <h2>
         I like <span className="text-gradient font-medium">sharing</span> what I learn
       </h2>
@@ -16,6 +19,14 @@ const Blog = ({ upTo = articles.length }: BlogProps) => {
           <BlogCard key={index} {...articles} />
         ))}
       </div>
+      {upTo !== articles.length && (
+        <button
+          onClick={() => navigate("/blog")}
+          className="btn btn-lg bg-gray-400 bg-opacity-10 shadow-lg text-gray-200 text-shadow hover:bg-gray-500 hover:bg-opacity-30"
+        >
+          View All
+        </button>
+      )}
     </div>
   );
 };
--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { projects } from "../../Assets/Data/projects/projects";
 import ProjectCard from "./ProjectCard";
 
@@ -6,16 +7,32 @@ interface ProjectsProps {
 }
 
 const Projects = ({ upTo = Object.keys(projects).length }: ProjectsProps) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex flex-col items-center ~px-4/12 gap-10">
+    <div
+      className={`flex flex-col items-center ~px-4/12 gap-10 ${
+        upTo === Object.keys(projects).length || "pb-[5vh]"
+      }`}
+    >
       <h2>
         I like <span className="text-gradient font-medium">making</span> things
       </h2>
       <div className="flex flex-col items-center gap-10 w-[80vw] md:w-[60vw]">
-        {Object.keys(projects).slice(0, upTo).map((project, index) => (
-          <ProjectCard key={index} project={project} />
-        ))}
+        {Object.keys(projects)
+          .slice(0, upTo)
+          .map((project, index) => (
+            <ProjectCard key={index} project={project} />
+          ))}
       </div>
+      {upTo !== Object.keys(projects).length && (
+        <button
+          onClick={() => navigate("/projects")}
+          className="btn btn-lg bg-gray-300 bg-opacity-10 shadow-lg text-gray-200 text-shadow hover:bg-gray-500 hover:bg-opacity-30"
+        >
+          View All Projects
+        </button>
+      )}
     </div>
   );
 };
