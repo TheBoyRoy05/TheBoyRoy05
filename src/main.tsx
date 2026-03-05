935b3b67925159d97939057e5670b3cbc4b119f4
Issac Roy <issacroy05@gmail.com>
2026-03-05 12:26:32 -0800

    fix: hash routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import App from "./App.tsx";
 import "./Styles/fonts.css";
@@ -9,9 +9,9 @@ import "./Styles/animations.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
       <Toaster />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
